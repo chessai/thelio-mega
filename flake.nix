6335d1b885558382fba5c07dad018ede6d100c92
chessai <chessai1996@gmail.com>
2023-05-16 08:25:53 -0700

    remove specialArgs

--- flake.nix
+++ flake.nix
@@ -10,14 +10,13 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware";
   };
 
-  outputs = { self, nixpkgs, disko, nixos-hardware, ... }@inputs:
+  outputs = { self, nixpkgs, disko, nixos-hardware, ... }:
   let
     system = "x86_64-linux";
   in
   {
     nixosConfigurations.thelio-mega = nixpkgs.lib.nixosSystem {
       inherit system;
-      specialArgs = inputs;
       modules = [
         nixos-hardware.nixosModules.system76
         disko.nixosModules.disko
